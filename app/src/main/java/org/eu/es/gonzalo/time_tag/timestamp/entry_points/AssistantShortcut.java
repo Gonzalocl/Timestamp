e843ec5f4d056bb3f6058392117ab82bacbf8f38
Gonzalo Caparrós Laiz <gonzalocl1024@gmail.com>
2022-03-26 22:24:10 +0100

    minimal app hello world

--- app/src/main/java/org/eu/es/gonzalo/time_tag/timestamp/entry_points/AssistantShortcut.java
+++ app/src/main/java/org/eu/es/gonzalo/time_tag/timestamp/entry_points/AssistantShortcut.java
@@ -5,10 +5,13 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 
+import org.eu.es.gonzalo.time_tag.timestamp.R;
+
 public class AssistantShortcut extends Activity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.assistant_view);
     }
 }
--- app/src/main/java/org/eu/es/gonzalo/time_tag/timestamp/entry_points/Launcher.java
+++ app/src/main/java/org/eu/es/gonzalo/time_tag/timestamp/entry_points/Launcher.java
@@ -5,10 +5,13 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 
+import org.eu.es.gonzalo.time_tag.timestamp.R;
+
 public class Launcher extends Activity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_view);
     }
 }

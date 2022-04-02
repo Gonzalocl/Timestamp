ad2c2a398a00bffa704d092f777992c8ee03744f
Gonzalo Caparrós Laiz <gonzalocl1024@gmail.com>
2022-04-02 04:26:15 +0200

    Configuration

---
+++ app/src/main/java/org/eu/es/gonzalo/time_tag/timestamp/app/domain/Configuration.java
@@ -0,0 +1,26 @@
+package org.eu.es.gonzalo.time_tag.timestamp.app.domain;
+
+public class Configuration {
+
+    public static final String BOT_API_TOKEN_SETTING_ID = "BOT_API_TOKEN";
+    public static final String BOT_USER_CHAT_ID_SETTING_ID = "BOT_USER_CHAT_ID";
+
+    private String botApiToken;
+    private String botUserChatId;
+
+    public String getBotApiToken() {
+        return botApiToken;
+    }
+
+    public void setBotApiToken(String botApiToken) {
+        this.botApiToken = botApiToken;
+    }
+
+    public String getBotUserChatId() {
+        return botUserChatId;
+    }
+
+    public void setBotUserChatId(String botUserChatId) {
+        this.botUserChatId = botUserChatId;
+    }
+}

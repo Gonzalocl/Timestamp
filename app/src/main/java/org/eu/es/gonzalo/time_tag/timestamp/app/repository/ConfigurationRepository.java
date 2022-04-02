1fdb30cdea43ac1a3786b0c408f9a21d77c3d607
Gonzalo Caparrós Laiz <gonzalocl1024@gmail.com>
2022-04-02 11:37:26 +0200

    TelegramTimestampRepository

--- app/src/main/java/org/eu/es/gonzalo/time_tag/timestamp/app/repository/ConfigurationRepository.java
+++ app/src/main/java/org/eu/es/gonzalo/time_tag/timestamp/app/repository/ConfigurationRepository.java
@@ -5,5 +5,4 @@ import org.eu.es.gonzalo.time_tag.timestamp.app.domain.Configuration;
 public interface ConfigurationRepository {
     void save(Configuration configuration);
     Configuration get();
-
 }
---
+++ app/src/main/java/org/eu/es/gonzalo/time_tag/timestamp/io/telegram/TelegramTimestampRepository.java
@@ -0,0 +1,11 @@
+package org.eu.es.gonzalo.time_tag.timestamp.io.telegram;
+
+import org.eu.es.gonzalo.time_tag.timestamp.app.domain.Timestamp;
+import org.eu.es.gonzalo.time_tag.timestamp.app.repository.TimestampRepository;
+
+public class TelegramTimestampRepository implements TimestampRepository {
+    @Override
+    public void save(Timestamp timestamp) {
+
+    }
+}

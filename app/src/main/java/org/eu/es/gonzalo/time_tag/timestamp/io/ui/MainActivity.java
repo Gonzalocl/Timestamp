3cad1af5871c1586781a4aaf0b15ae22cb31223d
Gonzalo Caparrós Laiz <gonzalocl1024@gmail.com>
2022-04-03 00:26:41 +0200

    main_activity layout

--- app/src/main/java/org/eu/es/gonzalo/time_tag/timestamp/io/ui/MainActivity.java
+++ app/src/main/java/org/eu/es/gonzalo/time_tag/timestamp/io/ui/MainActivity.java
@@ -5,10 +5,13 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 
+import org.eu.es.gonzalo.time_tag.timestamp.R;
+
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
     }
 }

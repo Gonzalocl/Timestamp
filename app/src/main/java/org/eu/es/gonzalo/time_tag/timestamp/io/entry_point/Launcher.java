e9a410e376f9d86787903080a1fea2c98683c18d
Gonzalo Caparrós Laiz <gonzalocl1024@gmail.com>
2022-04-02 21:38:50 +0200

    MainActivity and ConfigurationActivity

--- app/src/main/java/org/eu/es/gonzalo/time_tag/timestamp/io/entry_point/Launcher.java
+++ app/src/main/java/org/eu/es/gonzalo/time_tag/timestamp/io/entry_point/Launcher.java
@@ -1,12 +1,13 @@
 package org.eu.es.gonzalo.time_tag.timestamp.io.entry_point;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 
-import org.eu.es.gonzalo.time_tag.timestamp.R;
 import org.eu.es.gonzalo.time_tag.timestamp.io.context.ContextInitializer;
+import org.eu.es.gonzalo.time_tag.timestamp.io.ui.MainActivity;
 
 public class Launcher extends Activity {
 
@@ -14,6 +15,6 @@ public class Launcher extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ContextInitializer.initializeAllContexts(this);
-        setContentView(R.layout.main_view);
+        startActivity(new Intent(this, MainActivity.class));
     }
 }
---
+++ app/src/main/java/org/eu/es/gonzalo/time_tag/timestamp/io/ui/MainActivity.java
@@ -0,0 +1,14 @@
+package org.eu.es.gonzalo.time_tag.timestamp.io.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}

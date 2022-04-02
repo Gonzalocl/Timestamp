325f4b18f4c01d47fb9c4da81298848dec8e4e00
Gonzalo Caparrós Laiz <gonzalocl1024@gmail.com>
2022-04-02 11:40:53 +0200

    configurations and controllers

---
+++ app/src/main/java/org/eu/es/gonzalo/time_tag/timestamp/app/configuration/ConfigurationConfiguration.java
@@ -0,0 +1,4 @@
+package org.eu.es.gonzalo.time_tag.timestamp.app.configuration;
+
+public class ConfigurationConfiguration {
+}
---
+++ app/src/main/java/org/eu/es/gonzalo/time_tag/timestamp/app/controller/TimestampController.java
@@ -0,0 +1,4 @@
+package org.eu.es.gonzalo.time_tag.timestamp.app.controller;
+
+public class TimestampController {
+}

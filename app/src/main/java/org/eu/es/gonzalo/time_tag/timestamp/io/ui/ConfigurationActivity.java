7e94ade431897bb4bed01c5f1fed5eeac1d23d76
Gonzalo Caparrós Laiz <gonzalocl1024@gmail.com>
2022-04-03 02:12:10 +0200

    ConfigurationActivity

--- app/src/main/java/org/eu/es/gonzalo/time_tag/timestamp/io/ui/ConfigurationActivity.java
+++ app/src/main/java/org/eu/es/gonzalo/time_tag/timestamp/io/ui/ConfigurationActivity.java
@@ -2,13 +2,22 @@ package org.eu.es.gonzalo.time_tag.timestamp.io.ui;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+
+import org.eu.es.gonzalo.time_tag.timestamp.R;
 
 public class ConfigurationActivity extends Activity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.configuration_activity);
+    }
+
+    public void onClickSaveButton(View view) {
+        Toast.makeText(this, "onClickSaveButton", Toast.LENGTH_SHORT).show();
     }
 }

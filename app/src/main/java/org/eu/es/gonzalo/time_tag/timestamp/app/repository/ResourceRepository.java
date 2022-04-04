aeb641cf5af312522361ed90d66837c85dcfba21
Gonzalo Caparrós Laiz <gonzalocl1024@gmail.com>
2022-04-04 03:24:15 +0200

    async callback and error view

---
+++ app/src/main/java/org/eu/es/gonzalo/time_tag/timestamp/app/repository/ResourceRepository.java
@@ -0,0 +1,4 @@
+package org.eu.es.gonzalo.time_tag.timestamp.app.repository;
+
+public interface ResourceRepository {
+}
---
+++ app/src/main/java/org/eu/es/gonzalo/time_tag/timestamp/io/resource/AndroidResourceRepository.java
@@ -0,0 +1,6 @@
+package org.eu.es.gonzalo.time_tag.timestamp.io.resource;
+
+import org.eu.es.gonzalo.time_tag.timestamp.app.repository.ResourceRepository;
+
+public class AndroidResourceRepository implements ResourceRepository {
+}

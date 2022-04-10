0dfe47d2324ce4fc33b7f7441934707337cf2596
Gonzalo Caparrós Laiz <gonzalocl1024@gmail.com>
2022-04-10 18:50:59 +0200

    no ui

--- app/src/main/java/org/eu/es/gonzalo/time_tag/timestamp/io/ui/AssistantShortcutActivity.java
+++ app/src/main/java/org/eu/es/gonzalo/time_tag/timestamp/io/ui/AssistantShortcutActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 
-import org.eu.es.gonzalo.time_tag.timestamp.R;
 import org.eu.es.gonzalo.time_tag.timestamp.io.context.ContextInitializer;
 
 public class AssistantShortcutActivity extends Activity {
@@ -14,7 +13,6 @@ public class AssistantShortcutActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ContextInitializer.initializeAllContexts(this);
-        setContentView(R.layout.assistant_view);
         UiUtil.sendTelegramBotMessageTimestamp();
         finish();
     }

466684fdaee07b2e2965a33c5a6141e907c7c952
Gonzalo Caparrós Laiz <gonzalocl1024@gmail.com>
2022-04-10 21:54:06 +0200

    main activity text

--- app/src/main/java/org/eu/es/gonzalo/time_tag/timestamp/io/ui/MainActivity.java
+++ app/src/main/java/org/eu/es/gonzalo/time_tag/timestamp/io/ui/MainActivity.java
@@ -4,11 +4,16 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
 import org.eu.es.gonzalo.time_tag.timestamp.R;
+import org.eu.es.gonzalo.time_tag.timestamp.app.configuration.PreferenceConfiguration;
+import org.eu.es.gonzalo.time_tag.timestamp.app.repository.PreferenceRepository;
 import org.eu.es.gonzalo.time_tag.timestamp.io.context.ContextInitializer;
+
+import java.util.Optional;
 
 public class MainActivity extends Activity {
 
@@ -17,6 +22,13 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         ContextInitializer.initializeAllContexts(this);
         setContentView(R.layout.main_activity);
+
+        PreferenceRepository preferenceRepository = PreferenceConfiguration.getPreferenceRepository();
+        Optional<String> last_timestamps = preferenceRepository.get(PreferenceRepository.Preference.LAST_TIMESTAMPS);
+        String timestampsText = last_timestamps.orElse("NO TIMESTAMPS");
+
+        TextView textView = findViewById(R.id.main_activity_text_view);
+        textView.setText(timestampsText);
     }
 
     public void onClickTimestampButton(View view) {

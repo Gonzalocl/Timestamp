e621a14c41c8d2aae6654d74f50d5db2ca34e73d
Gonzalo Caparrós Laiz <gonzalocl1024@gmail.com>
2022-04-14 21:39:58 +0200

    EditNumberPreference data store

--- app/src/main/java/org/eu/es/gonzalo/time_tag/timestamp/io/ui/EditNumberPreference.java
+++ app/src/main/java/org/eu/es/gonzalo/time_tag/timestamp/io/ui/EditNumberPreference.java
@@ -1,12 +1,15 @@
 package org.eu.es.gonzalo.time_tag.timestamp.io.ui;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.text.InputType;
 import android.util.AttributeSet;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.preference.EditTextPreference;
+import androidx.preference.PreferenceDataStore;
+import androidx.preference.PreferenceManager;
 
 public class EditNumberPreference extends EditTextPreference {
 
@@ -32,5 +35,22 @@ public class EditNumberPreference extends EditTextPreference {
 
     private void setNumberInputType() {
         setOnBindEditTextListener(editText -> editText.setInputType(InputType.TYPE_CLASS_NUMBER));
+        setPreferenceDataStore(new PreferenceDataStore() {
+            @Override
+            public void putString(String key, @Nullable String value) {
+                SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(EditNumberPreference.this.getContext());
+                sharedPreferences.edit().putLong(key, Long.parseLong(value)).apply();
+            }
+
+            @Nullable
+            @Override
+            public String getString(String key, @Nullable String defValue) {
+                if (defValue == null) {
+                    return null;
+                }
+                SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(EditNumberPreference.this.getContext());
+                return String.valueOf(sharedPreferences.getLong(key, Long.parseLong(defValue)));
+            }
+        });
     }
 }
